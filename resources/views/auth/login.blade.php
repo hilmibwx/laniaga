8f4ad609a97ef1f403e419462fea2dd4e420040f
Hilmi Hidayat <hilmihidayat175@gmail.com>
2021-10-03 22:52:36 +0700

    first commit

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,122 @@
+@extends('layouts.auth')
+
+@section('content')
+    <div class="page-content page-auth">
+      <div class="section-store-auth" data-aos="fade-up">
+        <div class="container">
+          <div class="row align-items-center row-login">
+            <div class="col-lg-6 text-center">
+              <img
+                src="/images/login-placeholder.png"
+                alt=""
+                class="w-50 mb-4 mb-lg-none"
+              />
+            </div>
+            <div class="col-lg-5">
+              <h2>
+                Belanja kebutuhan utama, <br />
+                menjadi lebih mudah
+              </h2>
+              <form class="mt-3">
+                <div class="form-group">
+                  <label>Email address</label>
+                  <input
+                    type="email"
+                    class="form-control w-75"
+                    aria-describedby="emailHelp"
+                  />
+                </div>
+                <div class="form-group">
+                  <label>Password</label>
+                  <input type="password" class="form-control w-75" />
+                </div>
+                <a
+                  class="btn btn-success btn-block w-75 mt-4"
+                  href="/login.html"
+                >
+                  Sign In to My Account
+                </a>
+                <a class="btn btn-signup w-75 mt-2" href="{{ route('register') }}">
+                  Sign Up
+                </a>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+<div class="container d-none">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Register') }}</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('register') }}">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Register') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\{HomeController, CartController, CategoryController, DashboardController, DashboardProductController, DashboardSettingController, DashboardTransactionController, DetailController};
+use App\Http\Controllers\Auth\RegisterController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('categories', [CategoryController::class, 'index'])->name('categories');
+Route::get('details/{id}', [DetailController::class, 'index'])->name('detail');
+Route::get('carts', [CartController::class, 'index'])->name('cart');
+Route::get('success', [CartController::class, 'success'])->name('success');
+
+Route::get('register/success', [RegisterController::class, 'success'])->name('register-success');
+
+Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+Route::get('dashboard/products', [DashboardProductController::class, 'index'])->name('dashboard-product');
+Route::get('dashboard/products/create', [DashboardProductController::class, 'create'])->name('dashboard-product-create');
+Route::get('dashboard/products/{id}', [DashboardProductController::class, 'details'])->name('dashboard-product-details');
+
+Route::get('dashboard/transactions', [DashboardTransactionController::class, 'index'])->name('dashboard-transactions');
+Route::get('dashboard/transactions/{id}', [DashboardTransactionController::class, 'details'])->name('dashboard-transactions-details');
+
+Route::get('dashboard/settings', [DashboardSettingController::class, 'store'])->name('dashboard-settings-store');
+Route::get('dashboard/account', [DashboardSettingController::class, 'account'])->name('dashboard-settings-account');
+
+Route::prefix('admin')->namespace('admin')->group( function(){
+    Route::get('/', [App\Http\Controllers\Admin\DashboardController::class, 'index'])->name('admin.dashboard');
+});
+
+
+Auth::routes();
